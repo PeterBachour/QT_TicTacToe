ee588d37acf2069fee2d97f2e592527761363d6a
PeterBachour <peter_bachour@icloud.com>
2020-06-24 08:49:57 +0200

    [MOD] bug fix

--- mainwindow.h
+++ mainwindow.h
@@ -15,20 +15,20 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-    bool checkwin(char player);
-    void checkmove(int x, int y);
-    void initializeSpots();
-    void enableSpots(bool x);
-    void showSpots();
-    void drawWinLine();
-    bool checkDraw();
-    bool showEndGame();
-    bool isAvailableSpot(int x, int y);
-    void newGame();
-    void minimax(char AIboard[3][3]);
-    int minSearch(char AIboard[3][3]);
-    int maxSearch(char AIboard[3][3]);
-    int score();
+    bool checkwin(char player);              //checks if a player won
+    void checkmove(int x, int y);            //checks if the move is available
+    void initializeSpots();                  //initializes all spots to initial value
+    void enableSpots(bool x);                // enable all spots
+    void showSpots();                        // shows all spots
+    void drawWinLine();                      // draws line if all values are equal
+    bool checkDraw();                        // checks if the game ends with a draw
+    bool showEndGame();                      // shows the winner
+    bool isAvailableSpot(int x, int y);      // checks if a spot is available
+    void newGame();                          // launches a new game
+    void minimax(char AIboard[3][3]);        // minimax algorithm for AI
+    int minSearch(char AIboard[3][3]);       // minSearch algorithm for AI
+    int maxSearch(char AIboard[3][3]);       // maxSearch algorithm for AI
+    int score();                             // calculates the score for the minimax algorithm
 
 
 
@@ -42,8 +42,8 @@ private slots:
     void on_place_6_clicked();
     void on_place_7_clicked();
     void on_place_8_clicked();
-    void on_CB_darkmode_clicked();
-    void on_PB_new_game_clicked();
+    void on_CB_darkmode_clicked();           // dark mode button
+    void on_PB_new_game_clicked();           // new game button
 
 
 private:
