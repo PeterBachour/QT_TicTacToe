e9e07d06c409f2866e30f20f8bc8cc3c0ed9115e
PeterBachour <peter_bachour@icloud.com>
2020-06-24 05:03:54 +0200

    [ADD] icon

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/images/small_logo_100px.png"));
     MainWindow w;
     w.show();
     return a.exec();
